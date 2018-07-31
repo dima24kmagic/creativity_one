6fd07371c39484033268988b8df4805131116a6a
Dima Baranov <vajnoe-dima@mail.ru>
2018-07-31 18:21:12 +0300

    playing with gsap animation

--- src/js/center.js
+++ src/js/center.js
@@ -1,4 +1,4 @@
-import { TweenLite } from 'gsap';
+import { TweenLite, TimelineMax } from 'gsap';
 
 export default element => {
   //   TweenLite.to(element, 0.75, { transform: `translateY(-250px)` });
@@ -11,6 +11,10 @@ export default element => {
   //   );
   //   TweenLite.to(element, 0.75, { transform: `scale(5)` });
   //   TweenLite.fromTo(element, 1, { borderRadius: '100%' }, { borderRadius: '2px', delay: 1.5 });
-  TweenLite.to(element, 1, { transform: `translateY(-250px)` });
-//   TweenLite.fromTo(element, 1, { transform: `translateY(-250px)`, delay: 1 }, {marginLeft: 55, delay: 1});
+  let tl = new TimelineMax();
+  tl.to(element, 0.2, { transform: `translateY(-120px)` });
+  tl.to(element, 0.35, { transform: `translateY(-170px) scale(4)` });
+  tl.to(element, 0.75, { transform: `translateY(-0px) scale(4)` });
+  tl.to(element, 0.25, { borderRadius: "4px" });
+  //   TweenLite.fromTo(element, 1, { transform: `translateY(-250px)`, delay: 1 }, {marginLeft: 55, delay: 1});
 };
