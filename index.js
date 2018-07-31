8efd6af76b5e0fb9c18f5c71a5368b490dcea992
Dima Baranov <vajnoe-dima@mail.ru>
2018-07-31 16:26:37 +0300

    added challenges scss

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import { createVerticalWord } from './src/js/vertical-word';
+import { createTunnel } from './src/js/tunnel';
 
+//Create Vertical Word
 const verticalWord = document.querySelector('.vertical-word');
 verticalWord.appendChild(createVerticalWord('Find out the solution'));
+
+//Create Tunnel
+const tunnel = document.querySelector('.challenge--tunnel');
+tunnel.appendChild(createTunnel(5, 15));
---
+++ src/js/tunnel.js
@@ -0,0 +1,12 @@
+export const createTunnel = (squares = 5, gapBetween = 10) => {
+  const tunnelHolder = document.createElement('div');
+  for (let i = 0; i < squares; i++) {
+    let tunnelSquare = document.createElement('div');
+    tunnelSquare.classList.add('challenge--tunnel__square');
+    console.log(i * 5);
+    tunnelSquare.style.cssText = `top: ${i * gapBetween}px;
+    animation: tunnel-light 1s ease ${i * 0.05}s infinite;`;
+    tunnelHolder.appendChild(tunnelSquare);
+  }
+  return tunnelHolder;
+};
